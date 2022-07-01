baf9fe88ab4951feefa0179ab0e781f2f2148be0
Andrew <andrewfairley95@gmail.com>
2022-07-01 11:15:34 +0100

    tests for venue class properties passes

--- tests/karaoke_venue_tests.py
+++ tests/karaoke_venue_tests.py
@@ -14,3 +14,12 @@ class TestVenue(unittest.TestCase):
     def test_venue_has_name(self):
         self.assertEqual("code clan karaoke", self.venue_1.name)
         self.assertEqual("Karaoke place", self.venue_2.name)
+
+
+    def test_venue_holds_money(self):
+        self.assertEqual(200, self.venue_1.till)
+        self.assertEqual(100, self.venue_2.till)
+
+    def test_venue_room_count(self):
+        self.assertEqual(10, self.venue_1.amount_of_rooms)
+        self.assertEqual(4, self.venue_2.amount_of_rooms)
