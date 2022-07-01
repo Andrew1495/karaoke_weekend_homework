f1b04d6d189a256b4415b850ff6c67189d63fc97
Andrew <andrewfairley95@gmail.com>
2022-07-01 12:14:16 +0100

    guest class tested and properties made

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from tests.karaoke_venue_tests import TestVenue
-# from tests.guest_test import TestGuest
+from tests.guest_tests import TestGuest
 from tests.room_tests import TestRoom
 # from tests.song_test import TestSong
 
--- tests/guest_tests.py
+++ tests/guest_tests.py
@@ -1,5 +1,24 @@
-# import unittest
+import unittest
+from src.guest import Guest
 
-# class TestGuest(unittest.test):
+class TestGuest(unittest.TestCase):
 
-#     def setUp  = 
+    def setUp(self):
+        self.guest1 = Guest("Bob", 25.00, "Yellow submarine")
+        self.guest2 = Guest("Frank", 50.00, "Hey Jude")
+
+
+
+    def test_guest_has_name(self):
+        self.assertEqual("Bob", self.guest1.name)
+        self.assertEqual("Frank", self.guest2.name)
+
+    def test_guest_has_wallet(self):
+        self.assertEqual(25.00, self.guest1.wallet)
+        self.assertEqual(50.00, self.guest2.wallet)
+
+
+    
+    def test_guest_has_fav_song(self):
+        self.assertEqual("Yellow submarine", self.guest1.fav_song)
+        self.assertEqual("Hey Jude", self.guest2.fav_song)
